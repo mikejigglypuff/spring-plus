89100c466f47faa1bc563fbb748bb6069f67038a
mikejigglypuff <k24555569@gmail.com>
2025-01-16 14:17:32 +0900

    ✨:: weather, modifiedAt Todo 검색 기능 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -15,7 +16,18 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
+        "LEFT JOIN t.user " +
+        "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+    // t.weather = null인 경우는 WeatherClient에서 배제하므로 고려하지 않음
+    @Query("SELECT t FROM Todo t " +
+        "LEFT JOIN FETCH t.user " +
+        "WHERE (t.weather LIKE %:weather%) AND " +
+        "t.modifiedAt >= coalesce(:startModifiedTime, t.modifiedAt) AND " +
+        "t.modifiedAt <= coalesce(:endModifiedTime, t.modifiedAt) " +
+        "ORDER BY t.modifiedAt DESC"
+    )
+    Page<Todo> findAllByWeatherAndModifiedAt(
+        Pageable pageable, String weather, LocalDateTime startModifiedTime, LocalDateTime endModifiedTime);
 }
